fc9c8da509fe7faf1026732c6b0391c19b2c6362
swaroop. <swaroopshri2019@gmail.com>
2026-09-16 23:32:05 +0530

    feat: add jumpland sfx and a way to play movement state based anims
    - fix muzzle flash: now uses FOV fix

---
+++ Source/ValorantMechanics/Player/PlayerSounds.h
@@ -0,0 +1,40 @@
+﻿// © 2026 swaroop. Personal Unreal Engine project inspired by VALORANT.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ValorantMechanics/Core/Shared/PlayerStates.h"
+#include "PlayerSounds.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FCharMovementSoundCue
+{
+    GENERATED_BODY()
+    
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<USoundBase*> soundVariations;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float volumeMin = 1.f, volumeMax = 1.f;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float pitchMin = 1.f, pitchMax = 1.f;
+};
+
+
+
+/**
+ * movement sound cues related to any player character 
+ */
+UCLASS(BlueprintType)
+class VALORANTMECHANICS_API UPlayerMovementSounds : public UDataAsset
+{
+    GENERATED_BODY()
+    
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TMap<EMovementState, FCharMovementSoundCue> cues;
+};
